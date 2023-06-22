86c35f523016b4084758d1c08dc5fdc635995791
ONYXIY <onxvnezakona@gmail.com>
2023-06-22 03:51:12 +0300

    refact

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './MyPosts.module.css';
 import Post from './Post/Post';
+import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../redux/State';
 
 const MyPosts = (props) => {
 
@@ -8,15 +9,17 @@ const MyPosts = (props) => {
 
   let newPostElement = React.createRef();
 
+
+
   let addPost = () => {
     // props.addPost();
-    props.dispatch({type: 'ADD-POST'})
+    props.dispatch(addPostActionCreator())
 
   }
   let onPostChange = () => {
     let text = newPostElement.current.value;
     // props.updateNewPostText(text);
-    let action = {type: 'UPDATE-NEW-POST-TEXT', newText: text };
+    let action = updateNewPostTextActionCreator(text);
     props.dispatch(action)
   }
   return (
--- src/redux/State.js
+++ src/redux/State.js
@@ -1,3 +1,7 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
+const SEND_MESSAGE = 'SEND-MESSAGE';
 let store = {
   _state: {
     profilePage: {
@@ -24,6 +28,7 @@ let store = {
         { id: 4, name: 'Gaga(Artem)' },
         { id: 5, name: 'Artem' }
       ],
+      newMessageBody: "",
       friends: [
         { id: 1, friend: 'Sanya' },
         { id: 2, friend: 'Vvados' },
@@ -41,7 +46,7 @@ let store = {
     this._callSubscriber = obs;
   },
   dispatch(action) {
-    if (action.type === 'ADD-POST') {
+    if (action.type === ADD_POST) {
       let newPost = {
         id: 5,
         message: this._state.profilePage.newPostText,
@@ -50,11 +55,40 @@ let store = {
       this._state.profilePage.posts.push(newPost);
       this._state.profilePage.newPostText = '';
       this._callSubscriber(this._state)
-    } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+    } else if (action.type === UPDATE_NEW_POST_TEXT) {
       this._state.profilePage.newPostText = action.newText;
       this._callSubscriber(this._state)
-    }
+    } else if (action.type === UPDATE_NEW_MESSAGE_BODY) {
+      this._state.dialogsPage.newMessageBody = action.body;
+      this._callSubscriber(this._state)
+    } else if (action.type === SEND_MESSAGE) {
+      let body = this._state.dialogsPage.newMessageBody;
+      this._state.dialogsPage.newMessageBody = '';
+      this._state.dialogsPage.messages.push({id: 6, message: body});
+      this._callSubscriber(this._state);
 
+    }
+  }
+}
+
+export const addPostActionCreator = () => {
+  return {
+    type: ADD_POST
+  }
+}
+export const updateNewPostTextActionCreator = (text) => {
+  return {
+    type: UPDATE_NEW_POST_TEXT, newText: text
+  }
+}
+export const sendMessageCreator = () => {
+  return {
+    type: ADD_POST
+  }
+}
+export const updateNewMessageBodyCreator = () => {
+  return {
+    type: ADD_POST
   }
 }
 export default store;
