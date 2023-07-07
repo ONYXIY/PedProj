c06b189737c1a82b414a0d6c56699da0f0024c3e
ONYXIY <onxvnezakona@gmail.com>
2023-07-07 16:53:59 +0300

    refak (added thunk for header, profile)

--- src/api/api.js
+++ src/api/api.js
@@ -12,27 +12,22 @@ const instance = axios.create({
 export const usersAPI = {
     getUsers(currentPage = 1, pageSize = 10) {
         return instance.get(`users?page=${currentPage}&count=${pageSize}`, {
-
         }).then(response => response.data)
     },
-    
-    follow(userId){
-        return instance.post(instance.baseURL + `follow/${userId}`)
+    follow(userId) {
+        return instance.post(`follow/${userId}`)
     },
-    unfollow(userId){
-        return instance.delete(instance.baseURL +`follow/${userId}`)
-    }
-
-}
-export const profileAPI ={
+    unfollow(userId) {
+        return instance.delete(`follow/${userId}`)
+    },
     getProfile(userId) {
         return instance.get(`profile/` + userId)
             .then(response => response.data)
-    },
+    }
 }
-export const headerAPI ={
-    getAuth(){
-    return instance.get(instance.baseURL +`auth/me`,{
-  }).then(response => response.data)
-}
+export const authAPI = {
+    getAuthMe() {
+        return instance.get(`auth/me`, {
+        }).then(response => response.data)
+    }
 }
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import Profile from './Profile';
 import { connect } from 'react-redux';
-import {setUserProfile} from '../../redux/profile-reducer';
+import {getUserProfile} from '../../redux/profile-reducer';
 import {useLocation, useNavigate, useParams} from "react-router-dom";
-import { profileAPI } from '../../api/api';
-
-
 
 
 class ProfileContainer extends React.Component {
@@ -15,9 +12,7 @@ class ProfileContainer extends React.Component {
     if (!userId) {
       userId = 2;
     }
-    profileAPI.getProfile(userId).then(data => {
-                this.props.setUserProfile(data);
-            });
+    this.props.getUserProfile(userId);
   }
   render() {
     return <div>
@@ -45,4 +40,4 @@ function withRouter(Component) {
   return ComponentWithRouterProp;
 }
 
-export default connect(mapStateToProps, {setUserProfile})(withRouter(ProfileContainer));
+export default connect(mapStateToProps, {getUserProfile})(withRouter(ProfileContainer));
